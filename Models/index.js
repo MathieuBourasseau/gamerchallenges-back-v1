e34095138562b753ce6598b531bd738bd6753fe2
LudivineLeLan <lelanludivine@gmail.com>
2026-02-25 16:46:47 +0100

    model index with relations tables OK

--- Models/index.js
+++ Models/index.js
@@ -5,12 +5,36 @@ import { User } from "./user.model.js";
 import { sequelize } from "./sequelize.client.js";
 
 
-// challenge + user
+// challenge - user
 User.hasMany(Challenge, {foreignKey: "user_id", as: "challenges"} );
 Challenge.belongsTo(User, {foreignKey: "user_id", as: "creator"});
 
-// challenge + game
+// challenge - game
 Challenge.belongsTo(Game, {foreignKey: "game_id", as: "game"});
 Game.hasMany(Challenge, {foreignKey: "game_id", as: "challenges"});
 
-// participation + user
+// participation - user
+Participation.belongsTo(User, {foreignKey: "user_id", as: "player"});
+User.hasMany(Participation, {foreignKey: "user_id", as: "participations"});
+
+
+// participation - challenge 
+Participation.belongsTo(Challenge, {foreignKey: "challenge_id", as: "challenge"});
+Challenge.hasMany(Participation, {foreignKey: "challenge_id", as: "participations"});
+
+
+// table de liaison vote_participation 
+Participation.belongsToMany(User, {
+  through: "vote_participation",
+  foreignKey: "participation_id",
+  otherKey: "user_id",
+  as: "voters"
+});
+User.belongsToMany(Participation, {
+  through: "vote_participation",
+  foreignKey: "user_id",
+  otherKey: "participation_id",
+  as: "votedParticipations"
+});
+
+export { Challenge, Game, Participation, User, sequelize}; 

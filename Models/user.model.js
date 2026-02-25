6f786cc8c595c25eb93ec2bbb4a8a3182a52f3d5
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-02-25 15:31:11 +0100

    feat: user model created

---
+++ Models/user.model.js
@@ -0,0 +1,57 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "./sequelize.client.js";
+
+export class User extends Model { }
+
+
+User.init(
+   {
+      username: {
+         type: DataTypes.STRING(50),
+         allowNull: false,
+         unique: false,
+      },
+
+      role: {
+         type: DataTypes.STRING(50),
+      },
+      email: {
+         type: DataTypes.STRING,
+         allowNull: false,
+         unique: true,
+      },
+      password: {
+         type: DataTypes.STRING(100),
+         allowNull: false,
+      },
+      avatar: {
+         type: DataTypes.STRING(100),
+         allowNull: true
+      },
+
+      favouriteGame: {
+         type: DataTypes.STRING(100),
+         allowNull: true
+      },
+
+      twitch: {
+         type: DataTypes.TEXT,
+         allowNull: true
+      },
+
+      youtube: {
+         type: DataTypes.TEXT,
+         allowNull: true
+      },
+
+      discord: {
+         type: DataTypes.TEXT,
+         allowNull: true
+      }
+   },
+   {
+      sequelize, 
+      modelName: "User",
+      tableName: "user", 
+   }
+);

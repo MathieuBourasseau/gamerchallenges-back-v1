ab218766db1614957b5f5dc78bd6b1543f76e6b0
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-02 16:01:55 +0100

    feat: contact middleware created

--- Routers/contact.router.js
+++ Routers/contact.router.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { newMessage } from "../Schemas/index.js";
 
 // Router to handle contact request 
 export const contactRouter = Router();
 
-contactRouter.post("/contact", contactController.sendMessage)
+// Post request protected by newMessage schema
+contactRouter.post("/contact", newMessage, contactController.sendMessage)

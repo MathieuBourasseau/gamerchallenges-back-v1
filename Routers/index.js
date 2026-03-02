08db6dc3d2c229074897311bf1a7c2be74a20b33
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-02 15:11:52 +0100

    feat: contactRouter added in index.js

--- Routers/index.js
+++ Routers/index.js
@@ -3,6 +3,7 @@ import { homeRouter } from "./home.router.js";
 import { challengeRouter } from "./challenge.router.js";
 import { gameRouter } from "./game.router.js";
 import { userRouter } from "./user.router.js";
+import { contactRouter } from "./contact.router.js";
 
 export const apiRouter = Router();
 
@@ -10,3 +11,4 @@ apiRouter.use(homeRouter);
 apiRouter.use(challengeRouter);
 apiRouter.use(gameRouter);
 apiRouter.use(userRouter);
+apiRouter.use(contactRouter);

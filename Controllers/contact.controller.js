3af98e5c155b84b82eaa5f10c6a367ff1a68b552
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-02 15:28:15 +0100

    feat: begining of sendMessage method created

--- Controllers/contact.controller.js
+++ Controllers/contact.controller.js
@@ -1,4 +1,11 @@
-export const contactController ={
+export const contactController = {
 
-    
+    // Method to send message from contact
+    sendMessage(req, res) {
+
+        // Extract data from front 
+        const { name, email, message } = req.body;
+        
+    }
+
 }

a018f604a8c4bfa111d27972bdbebf36e47a4cc1
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-03 10:32:21 +0100

    feat: transporter to connect to gmail added

--- Controllers/contact.controller.js
+++ Controllers/contact.controller.js
@@ -1,3 +1,15 @@
+import nodemailer from 'nodemailer';
+
+// Create transporter to specify to nodemailer which service will be used and how to connect with it
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
 export const contactController = {
 
     // Method to send message from contact

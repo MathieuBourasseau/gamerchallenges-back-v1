c1a984c2a73dcd6c12ea84bfb01703d6b10cbc7a
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-03 10:44:43 +0100

    feat: mailOptions created

--- Controllers/contact.controller.js
+++ Controllers/contact.controller.js
@@ -1,12 +1,13 @@
 import nodemailer from 'nodemailer';
+import 'dotenv/config';
 
 // Create transporter to specify to nodemailer which service will be used and how to connect with it
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.EMAIL_USER, // Adress selected to handle mail sent/received
+        pass: process.env.EMAIL_PASS // Password to authorize the connection to the mail provider
     }
 });
 
@@ -17,7 +18,22 @@ export const contactController = {
 
         // Extract data from front 
         const { name, email, message } = req.body;
-        
+
+        // Mail options
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_USER,
+            replyTo: email,
+            subject: `Nouveau message de ${name} - GamerChallenges`,
+            html: `
+                    <h2>Nouveau contact via le formulaire</h2>
+                    <p><strong>Nom :</strong> ${name}</p>
+                    <p><strong>Email :</strong> ${email}</p>
+                    <p><strong>Message :</strong></p>
+                    <p style="border-left: 3px solid #ccc; padding-left: 10px;">${message}</p>
+                  `
+        };
+
     }
 
 }

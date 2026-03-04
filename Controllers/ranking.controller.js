81e38afa46461077ea3b6f9c80bc1dfb08ca3387
LudivineLeLan <lelanludivine@gmail.com>
2026-03-04 11:27:43 +0100

    ranking controller & router

---
+++ Controllers/ranking.controller.js
@@ -0,0 +1,66 @@
+import { User, Participation } from "../Models/index.js";
+import { fn, col } from "sequelize";
+
+export const rankingController = {
+	async rankByParticipations(req, res) {
+		try {
+			const ranking = await User.findAll({
+				attributes: [
+					"id",
+					"username",
+					"avatar",
+					[fn("COUNT", col("participations.id")), "participationCount"],
+				],
+				include: [
+					{
+						model: Participation,
+						as: "participations",
+						attributes: [],
+					},
+				],
+				group: ["User.id"],
+				order: [[fn("COUNT", col("participations.id")), "DESC"]],
+			});
+
+			res.json(ranking);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ message: "Erreur serveur" });
+		}
+	},
+
+	async rankByVotes(req, res) {
+		try {
+			const ranking = await User.findAll({
+				attributes: [
+					"id",
+					"username",
+					"avatar",
+					[fn("COUNT", col("participations->voters.id")), "voteCount"],
+				],
+				include: [
+					{
+						model: Participation,
+						as: "participations",
+						attributes: [],
+						include: [
+							{
+								model: User,
+								as: "voters",
+								attributes: [],
+								through: { attributes: [] },
+							},
+						],
+					},
+				],
+				group: ["User.id"],
+				order: [[fn("COUNT", col("participations->voters.id")), "DESC"]],
+			});
+
+			res.json(ranking);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ message: "Erreur serveur" });
+		}
+	},
+};

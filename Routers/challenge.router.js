a96e94f43d13d145466e868ffc612db8090705d4
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-05 10:48:46 +0100

    feat: route to show one challenge created

--- Routers/challenge.router.js
+++ Routers/challenge.router.js
@@ -3,4 +3,8 @@ import { challengeController } from "../Controllers/challenge.controller.js";
 
 export const challengeRouter = Router();
 
+// Route to show all challenges
 challengeRouter.get("/challenges", challengeController.getAllChallenges);
+
+// Route to show details of one challenge 
+challengeRouter.get("/challenges/:id", challengeController.getOneChallenge);

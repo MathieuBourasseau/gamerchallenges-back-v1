260a4838a8b5a96d454b8422e8276e3b357560ec
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-05 15:59:04 +0100

    feat: positive added in checkIdSchema

--- Schemas/id.schema.js
+++ Schemas/id.schema.js
@@ -2,5 +2,5 @@ import Joi from "joi";
 
 // Validation schema for id
 export const checkIdSchema = Joi.object({
-    id: Joi.number().integer().required()
+    id: Joi.number().integer().positive().required()
 });

8a0f1898ec6e3a4b63db7b409740ff2961d90c86
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-05 14:34:31 +0100

    feat: validId middleware used in game.router.js

--- Routers/game.router.js
+++ Routers/game.router.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { gameController } from "../Controllers/game.controller.js";
+import { validId } from "../Middlewares/index.js";
 
 export const gameRouter = Router();
 
+// Route to show all games
 gameRouter.get("/games", gameController.getAllGames);
-gameRouter.get("/games/:id", gameController.getGameById);
+
+// Route to show the details of one game
+gameRouter.get("/games/:id", validId , gameController.getGameById);

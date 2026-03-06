2516bd954e80b40fbcc2fa4329f0ff94f9b3c6ad
LudivineLeLan <lelanludivine@gmail.com>
2026-03-06 11:51:21 +0100

    user deletion method

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,6 +1,26 @@
 import { User } from "../Models/index.js";
 
 export const userController = {
+	// modèle Blablabook pouvant servir pour la partie register/login
+	// async createUser(req, res) {
+	//   try {
+	//     const data = Joi.attempt(req.body, createUserSchema);
+	//     const user = await User.create(data);
+	//     res.status(201).json(user);
+	//   } catch (error) {
+	//     console.error(error);
+	//     res.status(500).json({ error: "Erreur lors de la création de l'utilisateur" });
+	//   }
+	// },
+
+	// async loginUser(req, res) {
+	//   const { username, password } = req.body;
+	//   const user = await User.findOne({ where: { username: username } });
+	//   if (!user || user.password !== password) {
+	//     return res.status(401).json({ error: 'Utilisateur non valide' });
+	//   }
+	// },
+
 	//  get all user infos
 	async getUserById(req, res) {
 		try {
@@ -24,6 +44,35 @@ export const userController = {
 		} catch (error) {
 			console.error(error);
 			res.status(500).json({ message: "Erreur serveur" });
+		}
+	},
+
+	// delete user
+	async deleteUser(req, res) {
+		try {
+			const { id } = req.params;
+			const authUserId = req.user.id;
+
+			if (parseInt(id) !== authUserId) {
+				return res
+					.status(403)
+					.json({ error: "Vous ne pouvez pas supprimer ce compte" });
+			}
+
+			const deletedCount = await User.destroy({ where: { id } });
+
+			if (deletedCount === 0) {
+				return res.status(404).json({ error: "Utilisateur non trouvé" });
+			}
+
+			return res.status(200).json({ message: "Compte supprimé avec succès" });
+		} catch (error) {
+			console.error(error);
+			return res
+				.status(500)
+				.json({
+					error: "Une erreur est survenue lors de la suppression du compte",
+				});
 		}
 	},
 };

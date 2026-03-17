9fc8a8a73c30fef5513222f838641863217507fc
LudivineLeLan <lelanludivine@gmail.com>
2026-03-17 14:05:40 +0100

    count method moved in participation & not challenge to be able to get the results in front

--- Controllers/participation.controller.js
+++ Controllers/participation.controller.js
@@ -1,7 +1,6 @@
 import { User, Challenge, Participation, Game } from "../Models/index.js";
 
 export const participationController = {
-
 	//  User's participations
 	async getParticipationsByUser(req, res) {
 		try {
@@ -40,17 +39,14 @@ export const participationController = {
 		}
 	},
 
-	// Get participations bound to a challenge 
+	// Get participations bound to a challenge
 	async getParticipationsByChallenge(req, res) {
-
 		try {
-
 			// Catch id from params
 			const { id } = req.params;
 
 			// Search challenge and participation bound to it
 			const challenge = await Challenge.findByPk(id, {
-
 				include: {
 					model: Participation,
 					as: "participations",
@@ -59,33 +55,35 @@ export const participationController = {
 					include: {
 						model: User,
 						as: "player",
-						attributes: ["id", "username", "avatar"]
-					}
-				}
+						attributes: ["id", "username", "avatar"],
+					},
+				},
 			});
 
-			// If the challenge does not exist 
+			// If the challenge does not exist
 			if (!challenge) {
-				console.error("L'id du challenge demandé n'existe pas.")
-				return res.status(404).json({ error: "Le challenge demandé n'existe pas." })
-			};
+				console.error("L'id du challenge demandé n'existe pas.");
+				return res
+					.status(404)
+					.json({ error: "Le challenge demandé n'existe pas." });
+			}
 
-			return res.status(200).json(challenge)
-
+			return res.status(200).json(challenge);
 		} catch (error) {
-
-			console.error("Erreur de serveur lors de la recherche d'une participation à un challenge", error.message);
-			return res.status(500).json({ error: "Un problème est survenu avec le serveur." });
-
+			console.error(
+				"Erreur de serveur lors de la recherche d'une participation à un challenge",
+				error.message,
+			);
+			return res
+				.status(500)
+				.json({ error: "Un problème est survenu avec le serveur." });
 		}
 	},
 
 	// Share participation with url
 	async shareParticipation(req, res) {
-
 		try {
-
-			const { id } = req.user
+			const { id } = req.user;
 
 			const { title, url } = req.body;
 
@@ -95,12 +93,18 @@ export const participationController = {
 				user_id: id,
 			});
 
-			return res.status(201).json({ message: "Votre vidéo a été ajoutée ! " })
-
+			return res.status(201).json({ message: "Votre vidéo a été ajoutée ! " });
 		} catch (error) {
-			console.error("Erreur lors du partage de la participation", error.message);
-			return res.status(500).json({ error: "Une erreur interne au serveur s'est produite. Veuillez réessayer ultérieurement." })
+			console.error(
+				"Erreur lors du partage de la participation",
+				error.message,
+			);
+			return res
+				.status(500)
+				.json({
+					error:
+						"Une erreur interne au serveur s'est produite. Veuillez réessayer ultérieurement.",
+				});
 		}
-	}
-
-}
+	},
+};

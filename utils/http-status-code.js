748200e5c80553b4f1389fef8fe2e32acdecbbfd
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-17 12:18:17 +0100

    feat: error messages and status added to authentication controller

--- utils/http-status-code.js
+++ utils/http-status-code.js
@@ -2,15 +2,19 @@ export const httpStatusCodes = {
 	OK: 200,
 	CREATED: 201,
 	REDIRECT: 301,
+	BAD_REQUEST: 400,
 	FORBIDDEN: 403,
 	NOT_FOUND: 404,
+	CONFLICT: 409,
 	SERVER_ERROR: 500
 };
 
 export const responseMessages = {
 	[httpStatusCodes.OK]: "Opération réussie.",
 	[httpStatusCodes.CREATED]: "Ressource créée avec succès.",
+	[httpStatusCodes.BAD_REQUEST]: "Requête invalide.",
 	[httpStatusCodes.FORBIDDEN]: "Accès refusé.",
 	[httpStatusCodes.NOT_FOUND]: "Ressource non trouvée.",
-	[httpStatusCodes.SERVER_ERROR]: "Une erreur interne est survenue."
+	[httpStatusCodes.CONFLICT]: "Conflit avec une ressource existante",
+	[httpStatusCodes.SERVER_ERROR]: "Une erreur interne au serveur est survenue."
 };
